5867a18d0508772e2bf526800ad85b5d02d1f776
Snyizer <77493707+Snyizer@users.noreply.github.com>
2021-01-15 16:17:22 +0300

    Add files via upload

---
+++ P1/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double a = 3;
+        double b = 4;
+        double с ;
+        с = Math.sqrt( a * a + b * b );
+        System.out.println ("c = "+ с );
+    }
+}
