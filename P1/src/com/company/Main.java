3f294099cd9aab80f55c8cb259fb0e38d4ba8f78
Snyizer <77493707+Snyizer@users.noreply.github.com>
2021-01-15 16:28:15 +0300

    Update Main.java

--- P1/src/com/company/Main.java
+++ P1/src/com/company/Main.java
@@ -3,10 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        double a = 3;
-        double b = 4;
-        double с ;
-        с = Math.sqrt( a * a + b * b );
-        System.out.println ("c = "+ с );
+       System.out.println ("Hai Java") ;
     }
 }
